ca17ec52db1117f42e516aa87bed86172832a7a7
Amman Sood <392075+asood92@users.noreply.github.com>
2021-04-28 22:03:23 -0400

    done

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SelectField, SubmitField, FloatField
+from wtforms import StringField, SelectField, SubmitField, FloatField, PasswordField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, Length, URL
-from grocery_app.models import ItemCategory, GroceryStore
+from wtforms.validators import DataRequired, Length, URL, ValidationError
+from grocery_app.models import ItemCategory, GroceryStore, User
 
 
 class GroceryStoreForm(FlaskForm):
@@ -15,6 +15,7 @@ class GroceryStoreForm(FlaskForm):
         "Address", validators=[DataRequired(), Length(min=10, max=80)]
     )
     submit = SubmitField("Submit")
+
 
 class GroceryItemForm(FlaskForm):
     """Form for adding/updating a GroceryItem."""
@@ -29,3 +30,26 @@ class GroceryItemForm(FlaskForm):
         "Store", query_factory=lambda: GroceryStore.query, allow_blank=False
     )
     submit = SubmitField("Submit")
+
+
+class SignUpForm(FlaskForm):
+    username = StringField(
+        "User Name", validators=[DataRequired(), Length(min=3, max=50)]
+    )
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                "That username is taken. Please choose a different one."
+            )
+
+
+class LoginForm(FlaskForm):
+    username = StringField(
+        "User Name", validators=[DataRequired(), Length(min=3, max=50)]
+    )
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Log In")
--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -1,7 +1,8 @@
-from grocery_app.forms import GroceryItemForm, GroceryStoreForm
+from grocery_app.forms import GroceryItemForm, GroceryStoreForm, LoginForm, SignUpForm
 from flask import Blueprint, request, render_template, redirect, url_for, flash
-from datetime import date, datetime
-from grocery_app.models import GroceryStore, GroceryItem, ItemCategory
+from grocery_app.models import GroceryStore, GroceryItem, User
+from grocery_app import bcrypt
+from flask_login import login_user, logout_user, login_required, current_user
 
 # Import app and db from events_app package so that we can run app
 from grocery_app import app, db
@@ -12,6 +13,55 @@ main = Blueprint("main", __name__)
 #           Routes                       #
 ##########################################
 
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/signup", methods=["GET", "POST"])
+def signup():
+    """
+    Allows the user to register a new account,
+    hashes the password before saving.
+    """
+    print("in signup")
+    form = SignUpForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
+        user = User(username=form.username.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account Created.")
+        print("created")
+        return redirect(url_for("auth.login"))
+    print(form.errors)
+    return render_template("signup.html", form=form)
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+    """ Simple logout route"""
+    logout_user()
+    return redirect(url_for("main.homepage"))
+
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    """
+    Login page for the user to sign in to their account,
+    verifies that the form is valid, the user exists and the
+    password hash matches before saving.
+    """
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=True)
+            next_page = request.args.get("next")
+            return redirect(next_page if next_page else url_for("main.homepage"))
+    return render_template("login.html", form=form)
+
 
 @main.route("/")
 def homepage():
@@ -21,6 +71,7 @@ def homepage():
 
 
 @main.route("/new_store", methods=["GET", "POST"])
+@login_required
 def new_store():
     """
     Presents a form for a new grocery store to the user for input,
@@ -29,7 +80,9 @@ def new_store():
     """
     form = GroceryStoreForm()
     if form.validate_on_submit():
-        new_store = GroceryStore(title=form.title.data, address=form.address.data)
+        new_store = GroceryStore(
+            title=form.title.data, address=form.address.data, created_by=current_user
+        )
         db.session.add(new_store)
         db.session.commit()
         flash("New store was created successfully.")
@@ -39,6 +92,7 @@ def new_store():
 
 
 @main.route("/new_item", methods=["GET", "POST"])
+@login_required
 def new_item():
     """
     Presents a form for a new grocery item to the user for input,
@@ -53,6 +107,7 @@ def new_item():
             category=form.category.data,
             photo_url=form.photo_url.data,
             store=form.store.data,
+            created_by=current_user,
         )
         db.session.add(new_item)
         db.session.commit()
@@ -63,6 +118,7 @@ def new_item():
 
 
 @main.route("/store/<store_id>", methods=["GET", "POST"])
+@login_required
 def store_detail(store_id):
     """
     Presents a form for a grocery store to the user for modification,
@@ -84,6 +140,7 @@ def store_detail(store_id):
 
 
 @main.route("/item/<item_id>", methods=["GET", "POST"])
+@login_required
 def item_detail(item_id):
     """
     Presents a form for a grocery item to the user for modification,
@@ -106,3 +163,26 @@ def item_detail(item_id):
 
     item = GroceryItem.query.get(item_id)
     return render_template("item_detail.html", item=item, form=form)
+
+
+@main.route("/add_to_shopping_list/<item_id>", methods=["POST"])
+@login_required
+def add_to_shopping_list(item_id):
+    """
+    Allows the user to save individual items to their
+    personal shopping list
+    """
+    item = GroceryItem.query.get(item_id)
+    current_user.shopping_list_items.append(item)
+    db.session.add(current_user)
+    db.session.commit()
+    flash("Item was added to your list.")
+
+    return redirect(url_for("main.shopping_list"))
+
+
+@main.route("/shopping_list")
+@login_required
+def shopping_list():
+    """ Simple route to show the user their shopping list """
+    return render_template("shopping_list.html", items=current_user.shopping_list_items)
